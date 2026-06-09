368da5a341cae31b0dffddd10322cc96d7914f4b
technovahub@2025 <technovahubcareer@gmail.com>
2026-06-09 17:17:16 +0530

    cors error fixed

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ app.use(
   cors({
     origin: [
        'http://localhost:3000',
+         'https://technovahub.in',
     'http://127.0.0.1:3000',
     'http://localhost:3001',
     'http://127.0.0.1:3001',
