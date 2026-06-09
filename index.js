50faa4e80455c466d14919da1420183e9fab759b
technovahub@2025 <technovahubcareer@gmail.com>
2026-06-09 18:11:54 +0530

    cors

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ app.use(
     origin: [
        'http://localhost:3000',
          'https://technovahub.in',
+           'https://www.technovahub.in',
     'http://127.0.0.1:3000',
     'http://localhost:3001',
     'http://127.0.0.1:3001',
